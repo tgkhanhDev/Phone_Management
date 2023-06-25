e4c50b88431b2c158be2e68041b30b17f0519e06
Trần Giang Khánh <trangiangkhanh175.work@gmail.com>
2023-06-25 14:41:15 +0700

    100% layout

--- Customer/controller/main.js
+++ Customer/controller/main.js
@@ -1,7 +1,18 @@
 const btn = document.querySelector('button.mobile-menu-button');
-const menu= document.querySelector('.mobile-menu');
+const menu = document.querySelector('.mobile-menu');
 
-btn.addEventListener('click' , () => {
+btn.addEventListener('click', () => {
     menu.classList.toggle("hidden");
     menu.classList.toggle("show");
 })
+
+//nav bar scroll:
+window.onscroll = function() {scrollFunc()};
+scrollFunc = () => {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("navbar").style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+        document.getElementById("navbar").style.transition = "1s"
+      } else {
+        document.getElementById("navbar").style.background = "transparent";
+      }
+}
